3b3368beb021eae8849c7d21b5356e52c7feaec9
Andres Quast <quast@hbz-nrw.de>
2016-03-09 08:16:56 +0100

    restructured folders

--- src/html/threed/js/ThreedRender.js
+++ src/html/threed/js/ThreedRender.js
@@ -1,8 +1,15 @@
  $(function(){
 
+  
   //create scene
   var scene = new THREE.Scene();
+  //setDefaultScene( scene );
+  tdo = new ThreedObject();
+  tdo.setDefaultScene( scene );
+  
   var spotlight = setSpotLight();
+  spotlight.castShadow = true;
+
   scene.add( spotlight );
   scene.add( setAmbientLight() );
   var camera = setCamera( scene );
@@ -13,54 +20,53 @@
   renderer.setSize( $('#viewbox').innerWidth() - 100, $('#viewbox').innerHeight() );
   
   
-  //scene.fog = new THREE.Fog( 0xefefef , 0.055, 90 );
+  function setDefaultScene( scene ){
+   //scene.fog = new THREE.Fog( 0xefefef , 0.055, 90 );
 
-  //create Plane
-  var planeGeometry = new THREE.PlaneGeometry( 80 , 50 );
-  var planeMaterial = new THREE.MeshLambertMaterial( { color : 0xEEEEEE } );
-  var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-  plane.rotation.x = -0.8;
+   
+   //create Plane
+   var planeGeometry = new THREE.PlaneGeometry( 80 , 50 );
+   var planeMaterial = new THREE.MeshLambertMaterial( { color : 0xEEEEEE } );
+   var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+   plane.rotation.x = -0.8;
   
-  //create stripe
-  var torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16  );
-  var material = new THREE.MeshPhongMaterial( { color: 0x3302ff } );
-  var stripe = new THREE.Mesh( torusGeometry, material );
+   //create stripe
+   var torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16  );
+   var material = new THREE.MeshPhongMaterial( { color: 0x3302ff } );
+   var stripe = new THREE.Mesh( torusGeometry, material );
 
-  //create cube
-  var cubeGeometry = new THREE.BoxGeometry( 10, 10, 10  );
-  var material = new THREE.MeshLambertMaterial( { color: 0xdfcf34 } );
-  var cube = new THREE.Mesh( cubeGeometry, material );
+   //create cube
+   var cubeGeometry = new THREE.BoxGeometry( 10, 10, 10  );
+   var material = new THREE.MeshLambertMaterial( { color: 0xdfcf34 } );
+   var cube = new THREE.Mesh( cubeGeometry, material );
 
-  plane.position.y = -18;
-  plane.position.z = 0;
+   plane.position.y = -18;
+   plane.position.z = 0;
   
-  cube.position.x = -15;
-  stripe.position.x = 15;
+   cube.position.x = -15;
+   stripe.position.x = 15;
   
-  cube.castShadow = true;
-  stripe.castShadow = true;
-  plane.receiveShadow = true;
-  spotlight.castShadow = true;
+   cube.castShadow = true;
+   stripe.castShadow = true;
+   plane.receiveShadow = true;
   
-  //add plane to scene
-  scene.add( plane );
+   //add plane to scene
+   scene.add( plane );
 
-  //add stripe to scene
-  scene.add( stripe );
+   //add stripe to scene
+   scene.add( stripe );
 
-  //add cube to scene
-  scene.add( cube );
-
-  //place camera outside the stripe
-  //camera.position.z = 50;
-      
+   //add cube to scene
+   scene.add( cube );
+   return scene;
+  }     
 
   var stats = initStats();
   var clock = new THREE.Clock();
 
   render();
 
-  //animate scene ???
+  // render the scene
   function render() {
    stats.update();
    var delta = clock.getDelta();
@@ -135,11 +141,11 @@
   
   //append Animation to Objects
   function setAnimation() {
-   stripe.rotation.x += 0.1;
-   stripe.rotation.y += 0.1;
+   //stripe.rotation.x += 0.1;
+   //stripe.rotation.y += 0.1;
        
-   cube.rotation.x += -0.001;
-   cube.rotation.y += -0.001;
+   //cube.rotation.x += -0.001;
+   //cube.rotation.y += -0.001;
   }
   
  
